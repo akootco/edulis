a4ad92b07ef9cf171cbfc9dea350f007129d0f86
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-02-19 14:23:40 -0800

    better cook recipes - flugin now using the plushie cook recipe builder lol - made builder better

--- src/main/kotlin/co/akoot/plugins/edulis/util/CreateRecipes.kt
+++ src/main/kotlin/co/akoot/plugins/edulis/util/CreateRecipes.kt
@@ -1,66 +1,30 @@
 package co.akoot.plugins.edulis.util
 
-import co.akoot.plugins.bluefox.util.TimeUtil.parseTime
 import co.akoot.plugins.edulis.Edulis.Companion.craftingConfig
 import co.akoot.plugins.edulis.Edulis.Companion.smokerConfig
 import co.akoot.plugins.edulis.util.CreateItem.getInput
 import co.akoot.plugins.edulis.util.CreateItem.getMaterial
+import co.akoot.plugins.plushies.util.builders.CookRecipe
 import co.akoot.plugins.plushies.util.builders.CraftRecipe
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger.logger
-import org.bukkit.Bukkit
-import org.bukkit.NamespacedKey
-import org.bukkit.inventory.CampfireRecipe
-import org.bukkit.inventory.FurnaceRecipe
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.SmokingRecipe
 
 object CreateRecipes {
 
     fun smeltingRecipes(key: String) {
         val result = smokerConfig.getString("$key.output") ?: return
         val input = smokerConfig.getString("$key.input") ?: return
-        val time = parseTime(smokerConfig.getString("$key.cookTime") ?: "10s", true).toInt()
-        val xp = smokerConfig.getDouble("$key.xp") ?: 0.7f
-
-        val smokerKey = NamespacedKey("edulis", "${key}_smoker")
-        val campfireKey = NamespacedKey("edulis", "${key}_campfire")
-        val furnaceKey = NamespacedKey("edulis", "${key}_furnace")
 
         val parts = result.split("/")
         val amount = parts.getOrNull(1)?.toIntOrNull() ?: 1
 
-        Bukkit.removeRecipe(furnaceKey)
-        Bukkit.addRecipe(
-            FurnaceRecipe(
-                furnaceKey,
-                getMaterial(parts[0], amount = amount, recipeName = key) ?: return,
-                getInput(input, key) ?: return,
-                xp.toFloat(),
-                time
-            )
-        )
-
-        Bukkit.removeRecipe(campfireKey)
-        Bukkit.addRecipe(
-            CampfireRecipe(
-                campfireKey,
-                getMaterial(parts[0], amount = amount, recipeName = key) ?: return,
-                getInput(input, key) ?: return,
-                xp.toFloat(),
-                time * 3
-            )
-        )
-
-        Bukkit.removeRecipe(smokerKey)
-        Bukkit.addRecipe(
-            SmokingRecipe(
-                smokerKey,
-                getMaterial(parts[0], amount = amount, recipeName = key) ?: return,
-                getInput(input, key) ?: return,
-                xp.toFloat(),
-                time / 2
-            )
-        )
+        CookRecipe.builder(
+            key,
+            getInput(input, key) ?: return,
+            getMaterial(parts[0], amount = amount, recipeName = key) ?: return,
+            smokerConfig.getString("$key.cookTime"),
+            smokerConfig.getDouble("$key.xp")
+        ).smelt("edulis").smoke("edulis")
     }
 
     fun craftingRecipes(key: String) {
