b3c7f712063135adbe2a330ccbb727b31c499e8c
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-12-28 12:35:08 -0800

    drinkable

--- src/main/kotlin/co/akoot/plugins/edulis/commands/EdibleCommand.kt
+++ src/main/kotlin/co/akoot/plugins/edulis/commands/EdibleCommand.kt
@@ -2,15 +2,17 @@ package co.akoot.plugins.edulis.commands
 
 import co.akoot.plugins.bluefox.api.FoxCommand
 import co.akoot.plugins.bluefox.api.FoxPlugin
+import co.akoot.plugins.bluefox.extensions.getPDC
 import co.akoot.plugins.bluefox.extensions.hasPDC
 import co.akoot.plugins.edulis.Edulis.Companion.key
 import co.akoot.plugins.edulis.util.Util.isFood
 import co.akoot.plugins.plushies.util.builders.FoodBuilder
 import co.akoot.plugins.plushies.util.builders.ItemBuilder
-import io.papermc.paper.datacomponent.DataComponentTypes
+import org.bukkit.Material
 import org.bukkit.command.CommandSender
+import org.bukkit.inventory.ItemStack
 
-class EdibleCommand(plugin: FoxPlugin) : FoxCommand(plugin, "edible") {
+class EdibleCommand(plugin: FoxPlugin) : FoxCommand(plugin, "edible", aliases = arrayOf("drinkable")) {
 
     override fun onTabComplete(sender: CommandSender, alias: String, args: Array<out String>): MutableList<String> =
         mutableListOf()
@@ -19,24 +21,29 @@ class EdibleCommand(plugin: FoxPlugin) : FoxCommand(plugin, "edible") {
         val player = playerCheck(sender) ?: return false
         val item = player.inventory.itemInMainHand
         val edibleKey = key("edible")
-
-        if (item.isEmpty || item.isFood) return false
+        val bucketKey = key("bkt")
+        if (item.isEmpty || (item.isFood || item.type.isEdible)) return false
 
         if (item.itemMeta.hasPDC(edibleKey)) {
-            ItemBuilder.builder(item)
-                .removepdc(edibleKey)
-                .resetData(DataComponentTypes.CONSUMABLE)
-                .resetData(DataComponentTypes.FOOD)
-                .build()
+            val bucketType = item.itemMeta.getPDC<String>(bucketKey)
+            val material = bucketType?.let { Material.getMaterial(it) } ?: item.type
+            player.inventory.setItemInMainHand(ItemStack(material, item.amount))
             return true
         } else {
-            val edibleItem = ItemBuilder.builder(item)
-                .pdc(edibleKey)
+            val isBucket = item.type.name.endsWith("_BUCKET")
+            val edibleItem = ItemBuilder.builder(if (isBucket) ItemStack(Material.MILK_BUCKET) else item)
+                .pdc(edibleKey, true)
+                .copyOf(item)
+                .apply { if (isBucket) {
+                    itemModel(item.type.name.lowercase())
+                    pdc(bucketKey, item.type.name)
+                } }
                 .build()
 
-            FoodBuilder.builder(edibleItem)
+            player.inventory.setItemInMainHand(FoodBuilder.builder(edibleItem)
                 .hunger(2, 2.0f)
-                .isSnack().build()
+                .apply { if (alias == "drinkable") eatSound("entity.generic.drink") }
+                .isSnack().build())
             return true
         }
     }
