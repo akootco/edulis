6486bda422fb0ea75b1ea5635e2064b2c9c9a0e8
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2026-08-03 00:21:08 -0700

    update FoodItemMenu to use new api

--- src/main/kotlin/co/akoot/plugins/edulis/listeners/PluginEvent.kt
+++ src/main/kotlin/co/akoot/plugins/edulis/listeners/PluginEvent.kt
@@ -1,24 +1,11 @@
 package co.akoot.plugins.edulis.listeners
 
-import co.akoot.plugins.edulis.gui.FoodItemMenu
-import co.akoot.plugins.edulis.gui.FoodItemMenu.Companion.foodItemMenu
 import co.akoot.plugins.edulis.util.Util.registerEdulisRecipes
 import io.papermc.paper.event.server.ServerResourcesReloadedEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.inventory.InventoryClickEvent
 
 class PluginEvent: Listener {
-    @EventHandler
-    fun InventoryClickEvent.onInvClick() {
-        when (val holder = clickedInventory?.holder) {
-            is FoodItemMenu -> {
-                foodItemMenu(currentItem ?: return, whoClicked, holder)
-                isCancelled = true
-            }
-        }
-    }
-
     @EventHandler
     fun ServerResourcesReloadedEvent.orReload() {
         registerEdulisRecipes()
