a0b00cb099f53e9f9b32a9c443e9f694a251b4dc
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2026-08-25 12:26:17 -0700

    better ChestMenu page handling

--- src/main/kotlin/co/akoot/plugins/edulis/gui/FoodItemMenu.kt
+++ src/main/kotlin/co/akoot/plugins/edulis/gui/FoodItemMenu.kt
@@ -1,18 +1,12 @@
 package co.akoot.plugins.edulis.gui
 
-import co.akoot.plugins.bluefox.util.text
 import co.akoot.plugins.edulis.util.Util.foodid
 import co.akoot.plugins.edulis.util.Util.isFood
 import co.akoot.plugins.plushies.api.ChestMenu
 import co.akoot.plugins.plushies.util.Items.customItems
+import net.kyori.adventure.text.Component
 
-class FoodItemMenu(page: Int = 1) : ChestMenu(page) {
-
-    override val title = text("Custom Items")
-
+class FoodItemMenu() : ChestMenu() {
+    override val title = Component.text("Food")
     override val items = customItems.values.filter { it.isFood && !it.foodid.contains("bat_wing") }
-
-    override fun nextPage() = FoodItemMenu(page + 1)
-
-    override fun prevPage() = FoodItemMenu(page - 1)
 }
